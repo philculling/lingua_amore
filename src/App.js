cff53ec95b598a68ba07fad920b264c6be24bf41
Nai <nailahmukhtar@Nailahs-Air.home>
2023-03-21 23:33:12 +0000

    installed mui package and setup, imported container component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Container } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Container>
+    </Container>
   );
 }
 
