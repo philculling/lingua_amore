6690d4e9338c3e928fa4bcfc47b711ef9b1678c7
Musleha <86237540+Musleha1@users.noreply.github.com>
2023-03-22 20:20:22 +0000

    Added routes and components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavTabs from './components/NavTabs';
+import Home from './components/pages/Home';
+import French from './components/pages/French';
+import Spanish from './components/pages/Spanish';
+import Italian from './components/pages/Italian';
+
 import './App.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -5,10 +12,21 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { Container } from '@mui/material';
 
+
+
 function App() {
   return (
-    <Container>test
-    </Container>
+    <Router>
+      <NavTabs />
+        <Container>
+        </Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="french" element={<French />} />
+            <Route path="spanish" element={<Spanish />} />
+            <Route path="italian" element={<Italian />} />
+          </Routes>
+    </Router>
   );
 }
 
