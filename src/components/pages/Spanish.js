e20176bb9d0340ea0f586d1b1186f75fe1895dcb
Phil Culling <philculling@hotmail.com>
2023-03-23 20:55:13 +0000

    add more content to French quiz; create Spanishquiz.json; link Spanishquiz.json to Spanish.js; create Italianquiz.json; link Italianquiz.json to Italian.js

--- src/components/pages/Spanish.js
+++ src/components/pages/Spanish.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Quiz from 'react-quiz-component';
+import quiz from './Spanishquiz.json';
 
 function Spanish() {
   return (
     <div>
       <h1>Spanish</h1>
+      <h2>Spanish Quiz</h2>
+      <Quiz quiz={quiz}/>
       <p>
-        
+        Colores
       </p>
     </div>
   );
