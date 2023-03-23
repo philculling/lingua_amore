1874e3edfd06b115395e0a1c1218f3a1994ca1bb
Phil Culling <philculling@hotmail.com>
2023-03-23 20:03:26 +0000

    Render quiz to French.js

--- src/components/pages/French.js
+++ src/components/pages/French.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { quiz } from './Quiz';
+import Quiz from 'react-quiz-component';
+import quiz from './Quiz';
 
 function French() {
   return (
     <div>
       <h1>French</h1>
+      <h2>French Quiz</h2>
+      <Quiz quiz={quiz}/>
       <p>
+        Colours
       </p>
     </div>
   );
