f6064cd279236d190e659adfa4b626149218563c
Musleha <86237540+Musleha1@users.noreply.github.com>
2023-03-24 19:06:54 +0000

    NavTabs and page links working with MUI

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,59 +1,226 @@
-import React from "react";
-import"../App.css"
+// import React from "react";
+// import"../App.css"
 
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 
-function NavTabs() {
+// function NavTabs() {
+//   return (
+//     <ul className="nav nav-tabs">
+//       <li className="nav-item">
+//       <NavLink
+//         to="/"
+//         end
+//         className={({ isActive }) =>
+//           isActive ? 'nav-link active' : 'nav-link'
+//         }
+//       >
+//         Home
+//         </NavLink>
+//       </li>
+//       <li className="nav-item">
+//         <NavLink
+//           to="french"
+//           className={({ isActive }) =>
+//             isActive ? 'nav-link active' : 'nav-link'
+//           }
+//         >
+//           French
+//         </NavLink>
+//       </li>
+//       <li className="nav-item">
+//         <NavLink
+//           to="spanish"
+//           end
+//           className={({ isActive }) =>
+//             isActive ? 'nav-link active' : 'nav-link'
+//           }
+//         >
+//           Spanish
+//         </NavLink>
+//       </li>
+//       <li className="nav-item">
+//         <NavLink
+//           to="italian"
+//           className={({ isActive }) =>
+//             isActive ? 'nav-link active' : 'nav-link'
+//           }
+//         >
+//           Italian
+//         </NavLink>
+//       </li>
+//     </ul>
+//   );
+// }
+
+
+// export default NavTabs;
+
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { Link } from 'react-router-dom';
+
+const pages = ['French', 'Spanish', 'Italian'];
+const settings = ['Home', 'Flashcard Mode', 'Quiz Mode', 'High Scores'];
+
+function NavBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-      <NavLink
-        to="/"
-        end
-        className={({ isActive }) =>
-          isActive ? 'nav-link active' : 'nav-link'
-        }
-      >
-        Home
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="french"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          French
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="spanish"
-          end
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Spanish
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="italian"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Italian
-        </NavLink>
-      </li>
-    </ul>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <FavoriteBorderIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            lingua amore
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">
+                  <Link style={{ textDecoration: "none"}} 
+                  to={`/${page}`}>{page}</Link>
+                  </Typography>
+                  
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <FavoriteBorderIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            lingua amore
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                <Link style={{ textDecoration: "none", color: "white"}}
+                to={`/${page}`}>{page}</Link>
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
-
-
-export default NavTabs;
-
-
-
+export default NavBar;
