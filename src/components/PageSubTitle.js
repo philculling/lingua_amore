0d02079b31fa8af44e3ed85dd16e44f6886bae92
Musleha <86237540+Musleha1@users.noreply.github.com>
2023-03-26 23:19:12 +0100

    Added Typography styling components

---
+++ src/components/PageSubTitle.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Typography } from "@mui/material";
+
+const PageSubTitle = ({subtitle}) => {
+  return (
+    <Typography variant="h2">{subtitle}</Typography>
+  )
+}
+
+export default PageSubTitle
