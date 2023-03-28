da5920213d2833fa49e50f6cef68558913c3d90f
Musleha <86237540+Musleha1@users.noreply.github.com>
2023-03-28 22:53:38 +0100

    changed navbar to 'static'

--- src/index.js
+++ src/index.js
@@ -2,46 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#95818d',
-          },
-          secondary: {
-            main: '#f44336',
-          },
-    },
-        typography: {
-          h1: {
-            fontSize: "2rem",
-            marginLeft: "10px"
-          },
-          body1: {
-            fontWeight: 500,
-          },
-          button: {
-            fontStyle: '',
-          },
-          h2: {
-            fontSize: "1.5rem",
-            paddingTop: "50px",
-            marginLeft: "10px"
-
-          },
-          body1: {
-            fontSize: "1rem",
-            paddingTop: "40px",
-            marginLeft: "10px"
-        }
-        },
-      
-  });
-  
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <ThemeProvider theme={theme}><App /></ThemeProvider>
-    
-);
+root.render(<App />);
