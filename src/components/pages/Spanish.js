e226ca645cd2ded6ee5fdd09a6c07fba5e593239
Phil Culling <philculling@hotmail.com>
2023-03-29 15:08:39 +0100

    adapt Spanish.js to be constructed same as French.js; content still to be changed

--- src/components/pages/Spanish.js
+++ src/components/pages/Spanish.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import frenchData from '../French.json'
+import spanishData from '../Spanish.json'
 import Quiz from 'react-quiz-component';
 import quiz from './Spanishquiz.json';
 import { Flashcard, FlashcardArray } from "react-quizlet-flashcard";
@@ -13,63 +13,76 @@ import Vert from '../../sounds/Vert.mp3';
 import Bleu from '../../sounds/Bleu.mp3';
 import Blanc from '../../sounds/Blanc.mp3';
 import Brun from '../../sounds/Brun.mp3';
-
 import PageTitle from "../PageTitle";
 import PageSubTitle from "../PageSubTitle";
+import { Box, Button, Container } from "@mui/material";
 
 function Spanish() {
-  const [currentIndex, setCurrentIndex] = useState(0);
 
-  function handleNextItem() {
-    setCurrentIndex((currentIndex + 1) % frenchData.length);
-  }
-  const currentItem = frenchData[currentIndex];
+  const [showFlash, setShowFlash] = useState(true);
+  const [currentCard, setCurrentCard] = useState(1);
+
+  const handleFlashClick = () => {
+    setShowFlash(true);
+  };
+
+  const handleQuizClick = () => {
+    setShowFlash(false);
+  };
 
   function play() {
-    if (currentIndex === 0) {
+    if (currentCard === 1) {
     new Audio(Rouge).play()}
-    else if (currentIndex === 1) {
+    else if (currentCard === 2) {
       new Audio(Orange).play()}
-    else if (currentIndex === 2) {
+    else if (currentCard === 3) {
       new Audio(Jaune).play()
-    }else if (currentIndex === 3) {
+    }else if (currentCard === 4) {
       new Audio(Vert).play()
-    }else if (currentIndex === 4) {
+    }else if (currentCard === 5) {
       new Audio(Bleu).play()
-    }else if (currentIndex === 5) {
+    }else if (currentCard === 6) {
       new Audio(Blanc).play()
-    }else if (currentIndex === 6) {
+    }else if (currentCard === 7) {
       new Audio(Noir).play()
-    }else if (currentIndex === 7) {
+    }else if (currentCard === 8) {
       new Audio(Brun).play()
-    }else if (currentIndex === 8) {
+    }else if (currentCard === 9) {
       new Audio(Rose).play()
     }else 
       new Audio(Gris).play()
     }
 
+    const handleCardChange = (index) => {
+      setCurrentCard(index);
+    };
+
   return (
     <div>
       <PageTitle title="Spanish"/>
       <PageSubTitle subtitle="Spanish Quiz"/>
-      <Quiz quiz={quiz}/>
-      <p>
-        Colores
-      </p>
-        <FlashcardArray cards={frenchData} />
-        <nav>
-          <button onClick={handleNextItem}>Next</button>
-          <button>Answer</button>
-        </nav>
-        {
-            <Flashcard
-              id={currentItem.id}
-              key={currentItem.id}
-              name={currentItem.french}
-              picture={currentItem.picture}
-            />
-        }
+      <PageSubTitle subtitle="Colores"/>
+      <Container sx={{display: "flex", flexDirection: 'column', justifyContent: "center", alignItems: "center"}}>
         <button onClick={play}>Play Spanish word</button>
+        <Box sx={{display: "flex", justifyContent: "space-around", alignItems: "center"}}>
+          <Button color="secondary"   sx={{margin: "10px"}}onClick={handleQuizClick} variant="contained" size="large">Quiz</Button>
+          <Button color="secondary" onClick={handleFlashClick} variant="contained" size="large">Flashcards</Button>
+        </Box>
+        
+        {showFlash ? (
+          <Box>
+          <FlashcardArray 
+            cards={spanishData}
+            onCardChange={handleCardChange}
+          />
+          </Box>
+        ) : (
+          <Box>
+            <h2>Spanish Quiz</h2>
+            <Quiz quiz={quiz}/>
+          </Box>
+        )}
+      </Container>
     </div>
   );
 }
