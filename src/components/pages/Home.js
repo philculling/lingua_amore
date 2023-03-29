cc2e4ce3a9a698787fa4c4f0daf3e08d7b9ac1a7
Nai <nailah@outlook.com>
2023-03-29 23:32:43 +0100

    change 'Home' to 'Lingua Amore'

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => (
 <div
 
 sx={{display: "flex", justifyContent: "center", alignItems: "center", paddingLeft: 20, paddingTop: 10}}>
-  <PageTitle title="Home"/>
+  <PageTitle title="Lingua Amore"/>
     <div>
       <PageParagraph p="Discover a brand new way to get language fluency with Lingua Amore. Start by learning new vocabulary with our innovative flashcards. Then test your knowledge with the handy language quiz!"/>
       <PageParagraph p="I want to learn..."/>
